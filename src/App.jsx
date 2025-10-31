e5ae1327890f225547f895b2e2d029ca06149084
Gulshan Yadav <gulshanyadav0029@gmail.com>
2025-10-31 22:35:38 +0530

    Fixed routing issue for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,50 @@
 
 //gpt
 
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./Components/Navbar/Navbar";
+// import Hero from "./Components/Hero/Hero";
+// import About from "./Components/About/About";
+// import Services from "./Components/Services/Services";
+// import MyWork from "./Components/MyWork/MyWork";
+// import Contact from "./Components/Contact/Contact";
+// import Footer from "./Components/Footer/Footer";
+// import MoreProjects from "./Components/MoreProjects/MoreProjects";
+// import Profile from "./Components/Profile/Profile";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         {/* Home Page */}
+//         <Route
+//           path="/"
+//           element={
+//             <>
+//               <Navbar />
+//               <Hero />
+//               <About />
+//               <Services />
+//               <MyWork />
+//               <Profile />
+//               <Contact />
+//               <Footer />
+//             </>
+//           }
+//         />
+
+//         {/* More Projects Page */}
+//         <Route path="/more-projects" element={<MoreProjects />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+// gpt
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
@@ -39,7 +83,7 @@ import Profile from "./Components/Profile/Profile";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/er-gulshan-portfolio">
       <Routes>
         {/* Home Page */}
         <Route
